11ced594d3074e5d8c463edb5d89bc9aca958883
Malaka98 <malaka.aruna14413@gmail.com>
2022-04-15 10:09:12 +0530

    feat

--- src/test/java/com/securityexample/demo/controller/UserControllerTest.java
+++ src/test/java/com/securityexample/demo/controller/UserControllerTest.java
@@ -1,14 +1,17 @@
 package com.securityexample.demo.controller;
 
 import com.securityexample.demo.AbstractTest;
+import com.securityexample.demo.dto.UserDTO;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,7 +38,22 @@ class UserControllerTest extends AbstractTest {
     }
 
     @Test
-    void saveUser() {
+    void saveUser() throws Exception {
+        String uri = "/api/user/save";
+
+        String inputJson = super.mapToJson(UserDTO.builder()
+                .id(null)
+                .name("sanduni")
+                .username("sanduni")
+                .password("123")
+                .role(new ArrayList<>())
+                .build());
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(201, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        log.info("==========> " + content);
     }
 
     @Test
