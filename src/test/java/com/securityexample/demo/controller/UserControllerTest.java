2d8c5a56279b874f2e707415bb302b25d2b449b8
Malaka98 <malaka.aruna14413@gmail.com>
2022-04-15 11:10:10 +0530

    feat

--- src/test/java/com/securityexample/demo/controller/UserControllerTest.java
+++ src/test/java/com/securityexample/demo/controller/UserControllerTest.java
@@ -1,8 +1,10 @@
 package com.securityexample.demo.controller;
 
 import com.securityexample.demo.AbstractTest;
+import com.securityexample.demo.dto.RoleDTO;
 import com.securityexample.demo.dto.UserDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -57,10 +59,32 @@ class UserControllerTest extends AbstractTest {
     }
 
     @Test
-    void saveRole() {
+    void saveRole() throws Exception {
+        String uri = "/api/role/save";
+        String inputJson = super.mapToJson(RoleDTO.builder()
+                .id(null)
+                .name("SUPPER_ADMIN")
+                .build());
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(201, status);
+
+        String content = mvcResult.getResponse().getContentAsString();
+        log.info("===========> " + content);
     }
 
     @Test
-    void saveRoleToUser() {
+    void saveRoleToUser() throws Exception {
+        String uri = "/api/role/addtouser";
+        JSONObject inputJson = new JSONObject();
+        inputJson.put("username", "sanduni");
+        inputJson.put("roleName", "SUPPER_ADMIN");
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON_VALUE).content(String.valueOf(inputJson))).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        log.info("============> " + content);
     }
 }
